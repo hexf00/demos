59974c7e7edbef0445de5284889ec2f58618b8ea
hexf00 <hexf00@gmail.com>
2020-07-07 21:43:03 +0800

    大纲基础

--- virtual-scroll/src/index.tsx
+++ virtual-scroll/src/index.tsx
@@ -1,18 +1,50 @@
 import a from "./a";
 import Vue from "vue";
 
+
+let bullet = Vue.component("bullet",{
+  props:{
+    value:{
+      type:String,
+      default(){
+        return ""
+      }
+    }
+  },
+  methods:{
+    onKeyUp(e:KeyboardEvent){
+      if(e.key == "Enter"){
+        this.$emit("new");
+        e.stopPropagation();
+        e.preventDefault();
+      }
+      
+    }
+  },
+  render(h){
+    return <li ref="el" contenteditable="true" onKeydown={this.onKeyUp}>*&nbsp;{this.value}</li>
+  }
+})
+
+
+
 new Vue({
   // el: "#app",
   data() {
     return {
-      a: 2,
+      a: ["123"],
+    }
+  },
+  methods:{
+    New(){
+      // console.log("New");
+      this.a.push("");
+
     }
   },
   render(h) {
     return <span>
-      <span>
-        {this.a}
-      </span>
+      {this.a.map(it=><bullet value={it} onNew={this.New} ></bullet>)}
     </span>
   }
 }).$mount("#app");
