a13d89ada144a0eed2765a7be0686556bdb4b8ec
hexf00 <hexf00@gmail.com>
2021-03-21 16:42:54 +0800

    feat: tableCell添加下拉框

--- allcat/src/themes/element-ui/components/ui/TableView/components/TableCell/TableCell.tsx
+++ allcat/src/themes/element-ui/components/ui/TableView/components/TableCell/TableCell.tsx
@@ -1,6 +1,6 @@
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { CreateElement } from 'vue'
-import { Input, MessageBox } from 'element-ui'
+import { Input, MessageBox, Select } from 'element-ui'
 import { IJSONRow } from '@/models/Table/Row'
 import { IJSONTableField } from '@/models/Table/TableField'
 import style from './index.module.scss'
@@ -10,6 +10,7 @@ import style from './index.module.scss'
 export default class TableCell extends Vue {
   $refs!: {
     input: Input
+    select: Select
   }
 
   @Prop(Object) row!: IJSONRow
@@ -33,8 +34,16 @@ export default class TableCell extends Vue {
 
       // 重置是否需要保存为需要
       this.isNeedSave = true
+
+      //需要nextTick否则触发时机存在问题
       this.$nextTick(() => {
-        this.$refs.input.select()
+        if (field.type === 'text') {
+          this.$refs.input.select()
+        } else if (field.type === 'select') {
+          this.$refs.select.focus()
+          const el = this.$refs.select.$el as HTMLElement
+          el.click()
+        }
       })
     }
   }
@@ -44,33 +53,72 @@ export default class TableCell extends Vue {
     row[field.id] = this.value
   }
 
+  onBlur() {
+    this.submit()
+    this.isEdit = false
+  }
+
   render(h: CreateElement) {
     const { row, field } = this
-    const value = row[field.id]
     if (this.isEdit) {
-      return <el-input ref="input" size="mini" type="text" vModel={this.value} on={{
-        blur: () => {
-          this.submit()
-          this.isEdit = false
-        },
-      }} nativeOn={{
-        click: (e: Event) => {
-          e.stopPropagation()
-        },
-        keyup: (e: KeyboardEvent) => {
-          const { code } = e
-          if (code === 'Escape') {
-            // 取消保存
-            this.isNeedSave = false
-            this.isEdit = false
-          } else if (code === 'Enter') {
-            // 保存
-            this.submit()
-            this.isEdit = false
-          }
-        },
-      }} />
+      if (field.type === 'text') {
+        return <el-input ref="input" size="mini" type="text" vModel={this.value} on={{
+          blur: () => this.onBlur(),
+        }} nativeOn={{
+          click: (e: Event) => {
+            e.stopPropagation()
+          },
+          keyup: (e: KeyboardEvent) => {
+            const { code } = e
+            if (code === 'Escape') {
+              // 取消保存
+              this.isNeedSave = false
+              this.isEdit = false
+            } else if (code === 'Enter') {
+              // 保存
+              this.submit()
+              this.isEdit = false
+            }
+          },
+        }} />
+      } else if (field.type === 'select') {
+
+        const selectOptions = field.selectOptions || []
+        return <el-select ref="select" vModel={this.value} size="mini"
+          on={{
+            clickoutside: () => {
+              this.isEdit = false
+            },
+          }}
+          nativeOn={{
+            click: (e: Event) => {
+              // e.stopPropagation()
+            },
+            keyup: (e: KeyboardEvent) => {
+              const { code } = e
+              if (code === 'Escape') {
+                // 取消保存
+                this.isNeedSave = false
+                this.isEdit = false
+              } else if (code === 'Enter') {
+                // 保存
+                this.submit()
+                this.isEdit = false
+              }
+            },
+          }}>
+          {selectOptions.map(item => <el-option
+            key={item}
+            label={item.value}
+            value={item.value}>
+          </el-option>)}
+        </el-select>
+      } else {
+        return <div>控件不全</div>
+      }
+
     } else {
+      const value = row[field.id]
       return <div class={style.preview} on={{
         dblclick: () => {
           this.isEdit = true
