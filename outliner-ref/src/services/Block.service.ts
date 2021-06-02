bbf21ab9dfbff1a1410dbf5dca015c65d4a0c5e6
hexf00 <hexf00@gmail.com>
2021-06-03 01:02:01 +0800

    feat:抽离计算引用树，退出编辑时候重新计算树

--- outliner-ref/src/services/Block.service.ts
+++ outliner-ref/src/services/Block.service.ts
@@ -14,26 +14,35 @@ export default class BlockService implements IBlockService {
 
   constructor(public data: TreeItem<IBlock>, public pageService: PageService) {
     this.children = data.children.map(it => new BlockService(it, pageService))
-    const match = data.value.matchAll(/!\[\[([\u4e00-\u9fa5a-zA-Z0-9]+?)\]\]/g)
+    this.refs = this.calcRefs()
+  }
+
+  /** 计算引用的块 */
+  calcRefs(): BlockService[] {
+    const match = this.data.value.matchAll(/!\[\[([\u4e00-\u9fa5a-zA-Z0-9]+?)\]\]/g)
 
     const ids: string[] = []
     let r
     for (r = match.next(); !r.done; r = match.next()) {
       const id = r.value[1]
 
-      pageService.blockDict[id] && ids.push(id)
+      this.pageService.blockDict[id] && ids.push(id)
     }
 
-    this.refs = ids.map(it => new BlockService(pageService.blockDict[it], pageService))
-
+    return ids.map(it => new BlockService(this.pageService.blockDict[it], this.pageService))
   }
 
   showEdit() {
     this.isEdit = true
   }
 
+  /**
+   * 退出编辑模式
+   * 如果用户修改了数据，需要刷新引用的块
+   */
   hideEdit() {
     this.isEdit = false
+    this.refs = this.calcRefs()
   }
 
 }
