130a95a77848aadd9dcfb68acac7288b08b784ad
hexf00 <hexf00@gmail.com>
2021-07-04 23:28:39 +0800

    feat:allcat引用id失效情况

--- allcat/src/themes/element-ui/components/ui/TableView/components/RelationCell/service.ts
+++ allcat/src/themes/element-ui/components/ui/TableView/components/RelationCell/service.ts
@@ -17,6 +17,11 @@ export default class RelationCellService extends BaseCellService {
 
   toText (value: string): string {
     const relationTable = store.currentApp!.tables[this.field.relationTo]
-    return relationTable.rows[value][relationTable.primaryField] as string || '-'
+    const row = relationTable.rows[value]
+    !row && console.warn('失效引用', value)
+    // 删除数据需要联动删除，否则这里会报错
+    // 表被删除
+    // 行被删除
+    return (row && row[relationTable.primaryField] as string) || '-'
   }
 }
