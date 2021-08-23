17182989813446fb26de54f557b330bf1f4eff3c
Gitea <gitea@fake.local>
2021-08-23 17:09:06 +0800

    feat:添加测试用例文件

---
+++ allcat/test/api.test.ts
@@ -0,0 +1,12 @@
+
+describe('操作测试', () => {
+  test('新建数据', () => {
+    expect(true).toBe(true)
+  })
+  test('修改数据', () => {
+    expect(true).toBe(true)
+  })
+  test('查询数据', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ allcat/test/op.test.ts
@@ -0,0 +1,13 @@
+
+describe('undo/redo测试', () => {
+
+  test('新建数据', () => {
+    expect(true).toBe(true)
+  })
+})
+
+describe('多终端测试', () => {
+  test('新建数据', () => {
+    expect(true).toBe(true)
+  })
+})
